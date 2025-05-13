eaeb4fc25f06454c9b916fb8a5f406beb85ba443
yonaka15 <yonaka@jugoya.ai>
2025-05-13 09:16:32 +0900

    add: permissions

--- subtitle/models.py
+++ subtitle/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
-
-class Meta:
-    # アプリケーションへのアクセス権限を定義
-    permissions = [
-        ("view_app", "Can view subtitle application"),
-    ]
+class App(models.Model):
+    pass
